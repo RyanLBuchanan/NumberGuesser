af96eff2b223e1d1c584a84ca4f603cb0c7acbe0
Ryan Lowell Buchanan <buchanan.ryan22@gmail.com>
2022-08-11 10:46:17 -0600

    feat: Added rules to game in js script

---
+++ scripts/app.js
@@ -0,0 +1,8 @@
+/*
+GAME FUNCTION:
+- Player must guess a number between a min and max
+- Player gets a certain number of guesses
+- Notify player of guesses remaining
+- Notify player of the correct answer if they lose
+- Let player choose to play again
+*/
