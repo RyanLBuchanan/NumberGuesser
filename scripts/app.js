ed0dd5093f247d0cc781c7d517c2d5aa7267a132
Ryan Lowell Buchanan <buchanan.ryan22@gmail.com>
2022-08-11 10:51:26 -0600

    feat: Began variables

--- scripts/app.js
+++ scripts/app.js
@@ -6,3 +6,12 @@ GAME FUNCTION:
 - Notify player of the correct answer if they lose
 - Let player choose to play again
 */
+
+// Game values
+let min = 1,
+    max = 10,
+    winningNum = 2,
+    guessesLeft = 3;
+
+// UI elements
+const game = document.getElementById('game');
